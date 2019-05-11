47a3f286d787c84b787952aaa011047bac91c41a
konapeso <n2kgt1@gmail.com>
2019-05-11 10:51:33 +0900

    add firebase

--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
+import firebase from "firebase";
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA3zGHEQY9MBgwGw3CN5jYzRJofapffTPo",
+  authDomain: "vf-sample.firebaseapp.com",
+  databaseURL: "https://vf-sample.firebaseio.com",
+  projectId: "vf-sample",
+  storageBucket: "vf-sample.appspot.com",
+  messagingSenderId: "1083235425086",
+  appId: "1:1083235425086:web:31a68d504d746d61"
+};
+
+
+firebase.initializeApp(firebaseConfig);
+
 
 Vue.config.productionTip = false;
 
